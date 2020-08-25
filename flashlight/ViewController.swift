f2a19cfee7dff2dc1664fedecdcb8d6fb605068e
fwani <seungfwani@gmail.com>
2020-08-26 00:05:04 +0900

    Update LaunchScreen & Add Sound and flash

--- flashlight/ViewController.swift
+++ flashlight/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,15 +15,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var flashImageView: UIImageView!
     
     var isOn = false
+    var switchSound: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        makeSound()
+    }
+
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
+
+    func makeSound() {
+        let path = Bundle.main.path(forResource: "switch.wav", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            switchSound = try AVAudioPlayer(contentsOf: url)
+            switchSound?.prepareToPlay()
+        } catch {
+            // couldn't load file :(
+        }
     }
 
     @IBAction func switchTapped(_ sender: Any) {
         isOn = !isOn
         
+        switchSound?.play()
+        toggleTorch(on: isOn)
 //        if isOn {
 //            switchButton.setImage(UIImage(named: "onSwitch"), for: .normal)
 //            flashImageView.image = UIImage(named: "onBG")
